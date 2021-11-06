ca83249b7ad66e59a84027bc1cee0fd701d6458d
Shilpa <shilpa.sns11@gmail.com>
2021-11-06 01:47:26 -0700

    Updates: Etag and conditional read implemented

--- Server/start.py
+++ Server/start.py
@@ -21,11 +21,11 @@ def readData(type,id):
         for key in r.scan_iter(type+'_'+id+'..*'):
             data[key.split(type+'_'+id+'..')[-1]] = json.loads(r.get(key))
 
-        # data = json.loads(r.get(type+'_'+id))
-
         response = Response(json.dumps(data))
         response.add_etag()
         (etag,is_weak) = response.get_etag()
+
+        print(etag,client_match_id)
 
         if etag == client_match_id:
             return Response(status=304, mimetype='application/json')
@@ -60,76 +60,119 @@ def readInput():
         key_prefix = content['objectType']+'_'+content['objectId']
 
         for key in content.keys():
-            print(key, key_prefix+'..'+key, content[key])
+            # print(key, key_prefix+'..'+key, content[key])
             r.set(key_prefix+'..'+key,json.dumps(content[key]))
 
+        data = {}
+        for key in r.scan_iter(key_prefix+'..*'):
+            data[key.split(key_prefix+'..')[-1]] = json.loads(r.get(key))
+
         # response = Response(json.dumps({'Message': 'Valid object found, stored in redis', 'redis-key': key, 'objectId': content['objectId'],'objectType': content['objectType']}), status=200, mimetype='application/json')
-        response = Response(json.dumps(content), status=200, mimetype='application/json')
+        response = Response(json.dumps(data), status=200, mimetype='application/json')
         response.add_etag()
-        # print(response.get_etag())
+        print(response.get_etag())
 
         return response
 
     except Exception as e:
         return Response(e.message, status=400, mimetype='text/plain')
 
-# Patch
-@app.route("/v1/<type>/<id>/jsonvalput",methods=["PUT"])
+# Put
+@app.route("/v1/<type>/<id>",methods=["PUT"])
 def readPutInput(type,id):
     content = request.json
     schema = json.load(open('../json_schema.json'))
     # Mark status code based on the status of validation
     try:
 
-        validate(instance=content, schema=schema)
+        client_match_id = request.headers.get('If-Match')
 
         # Extract ObjectId, ObjectType and generate key
         key_prefix = type+'_'+id
 
-        for key in content.keys():
-            r.set(key_prefix+'..'+key,json.dumps(content[key]))
+        data = {}
+        for key in r.scan_iter(key_prefix + '..*'):
+            data[key.split(key_prefix + '..')[-1]] = json.loads(r.get(key))
 
-        # response = Response(json.dumps({'Message': 'Valid object found, stored in redis', 'redis-key': key, 'objectId': content['objectId'],'objectType': content['objectType']}), status=200, mimetype='application/json')
-        response = Response(json.dumps(content), status=200, mimetype='application/json')
+        response = Response(json.dumps(data), status=200, mimetype='application/json')
         response.add_etag()
-        # print(response.get_etag())
+        (etag, weak) = response.get_etag()
 
-        return response
+        if client_match_id == etag:
+
+            validate(instance=content, schema=schema)
+
+            for key in content.keys():
+                r.set(key_prefix+'..'+key,json.dumps(content[key]))
+
+            data = {}
+            for key in r.scan_iter(key_prefix + '..*'):
+                data[key.split(key_prefix + '..')[-1]] = json.loads(r.get(key))
+
+            # response = Response(json.dumps({'Message': 'Valid object found, stored in redis', 'redis-key': key, 'objectId': content['objectId'],'objectType': content['objectType']}), status=200, mimetype='application/json')
+            response = Response(json.dumps(data), status=200, mimetype='application/json')
+            response.add_etag()
+            (etag,weak) = response.get_etag()
+            # print(etag)
+            return response
+
+        else:
+
+            response = Response(status=412, mimetype='application/json')
+            return response
 
     except Exception as e:
         return Response(e.message, status=400, mimetype='text/plain')
 
 # Patch
-@app.route("/v1/<type>/<id>/jsonvalpatch",methods=["PATCH"])
+@app.route("/v1/<type>/<id>",methods=["PATCH"])
 def readPatchInput(type,id):
     content = request.json
     schema = json.load(open('../json_schema.json'))
     # Mark status code based on the status of validation
     try:
 
+        client_match_id = request.headers.get('If-Match')
+
         # Extract ObjectId, ObjectType and generate key
         key_prefix = type+'_'+id
 
         data = {}
-        for key in r.scan_iter(key_prefix+'..*'):
-            data[key.split(key_prefix+'..')[-1]] = json.loads(r.get(key))
+        for key in r.scan_iter(key_prefix + '..*'):
+            data[key.split(key_prefix + '..')[-1]] = json.loads(r.get(key))
 
-        key = list(content.keys())[0]
-        for item in content[key]:
-            if item not in data[key]:
-                data[key].append(item)
-
-        validate(instance=data, schema=schema)
-
-        for key in content.keys():
-            r.set(key_prefix+'..'+key,json.dumps(data[key]))
-
-        # response = Response(json.dumps({'Message': 'Valid object found, stored in redis', 'redis-key': key, 'objectId': content['objectId'],'objectType': content['objectType']}), status=200, mimetype='application/json')
         response = Response(json.dumps(data), status=200, mimetype='application/json')
         response.add_etag()
-        # print(response.get_etag())
+        (etag, weak) = response.get_etag()
 
-        return response
+        if client_match_id == etag:
+
+            data = {}
+            for key in r.scan_iter(key_prefix+'..*'):
+                data[key.split(key_prefix+'..')[-1]] = json.loads(r.get(key))
+
+            key = list(content.keys())[0]
+            for item in content[key]:
+                if item not in data[key]:
+                    data[key].append(item)
+
+            validate(instance=data, schema=schema)
+
+            for key in content.keys():
+                r.set(key_prefix+'..'+key,json.dumps(data[key]))
+
+            # response = Response(json.dumps({'Message': 'Valid object found, stored in redis', 'redis-key': key, 'objectId': content['objectId'],'objectType': content['objectType']}), status=200, mimetype='application/json')
+            response = Response(json.dumps(data), status=200, mimetype='application/json')
+            response.add_etag()
+            (etag, weak) = response.get_etag()
+            # print(etag)
+
+            return response
+
+        else:
+
+            response = Response(status=412, mimetype='application/json')
+            return response
 
     except Exception as e:
         return Response(e.message, status=400, mimetype='text/plain')
